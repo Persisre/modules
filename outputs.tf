7d662113a50d57aca3b0f80ae6b6732bc197af19
Persisre <118957660+Persisre@users.noreply.github.com>
2022-11-24 11:17:20 +0530

    Delete outputs.tf
    s

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "project_id" {
-  description = "Project id"
-  value       = local.project_id
-}
-
-output "name" {
-  description = "The full resource name for the SLO"
-  value       = google_monitoring_slo.slo.name
-}
